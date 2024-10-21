9e7c179a508e27ed1233bf9017914fd8f4f60bc3
Hsingai Altaica <draltaica@yahoo.com>
2024-10-21 11:10:21 -0700

    better impl of polygon drawing

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -1,27 +1,21 @@
 use macroquad::{color::Color, math::Vec2, shapes::draw_triangle};
 
 pub fn draw_polygon(pos: Vec2, radius: f32, sides: usize, color:Color) {
-        let angle_step = 2.0 * std::f32::consts::PI / sides as f32;
+    let angle_step = 2.0 * std::f32::consts::PI / sides as f32;
+
+    let start = foo(0, angle_step, pos, radius);
     
-
-        let mut a = foo(0, angle_step, pos, radius);
-        for i in 1..sides {
-            let b = foo(i, angle_step, pos, radius);
-            draw_triangle(
-                pos,
-                a,
-                b,
-                color
-            );
-            a = b;
-
-        }
+    let mut a = foo(0, angle_step, pos, radius);
+    for i in 1..sides {
+        let b = foo(i, angle_step, pos, radius);
         draw_triangle(
-            pos,
+            start,
             a,
-            foo(0, angle_step, pos, radius),
+            b,
             color
         );
+        a = b;
+    }
 }
 
 fn foo(i: usize, angle_step: f32, pos: Vec2 ,radius: f32) -> Vec2{
